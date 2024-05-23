1facbce1787e76269b3082eab10f32f3ee2e1d1f
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-05-23 19:00:28 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -9,7 +9,7 @@ from pyrogram.types import Document, Video
 from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.errors import MessageNotModified
-from config import DOWNLOAD_LOCATION, CAPTION
+from config import DOWNLOAD_LOCATION, CAPTION, FILE_SIZE_LIMIT, STRING_SESSION,
 from main.utils import progress_message, humanbytes, upload_document, download_media
 import subprocess
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -22,10 +22,6 @@ FILE_SIZE_LIMIT = 2 * 1024 * 1024 * 1024  # 2 GB
 # Config class to hold your configurations
 class Config:
     STRING_SESSION = "BQCk-AgAXjv-rvP8Q3ZK7uVQ4bc6maTPaqtZ6iFqJDxWELtFu74RHoIvXfn6dLPZ9b1u9fWlV4ZqFJmWKm4jXrbw_LzfAeuYLbiFgdAcZ-mKa22oeHOZuN2iFFYzKCeDBOA4ZgJX74pQo8EliICqLxXI7Jo3gOPElzU3O11CS4kxFMGylSW_vSW9v6lTimUGXz4aW6Te-VkWLUmzQrvOiWaObizOe_y1dK3CXwNfCp0mzh1cDbTmGpAiHG5ShRC4Du2sAPudcnobX9hrPRKp5Ly0M0AOVnpJtKfh1zyOzDWjwoY9qA97hoyd1ITfNg8ZBHHe3a_2gKh2Lj85OWNANgL8doeABAAAAAGBoJ6aAA"
-
-# Helper function to convert bytes to a human-readable format
-def humanbytes(size):
-    return f"{size / (1024 * 1024):.2f} MB"
 
 # Initialize the string session client
 string_session_client = Client("my_session", api_id="10811400", api_hash="191bf5ae7a6c39771e7b13cf4ffd1279", session_string=Config.STRING_SESSION)
