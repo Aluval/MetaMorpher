2a9ae556e546425739565b5f8f549c40d0977381
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-05-23 19:28:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ SUNRISES_PIC= "https://graph.org/file/5966e801852b2bba18afb.jpg"  # Replace with
 FILE_SIZE_LIMIT = 2 * 1024 * 1024 * 1024  # 2 GB
 
 # Config class to hold your configurations
-class Config:
-    STRING_SESSION = "BQCk-AgAXjv-rvP8Q3ZK7uVQ4bc6maTPaqtZ6iFqJDxWELtFu74RHoIvXfn6dLPZ9b1u9fWlV4ZqFJmWKm4jXrbw_LzfAeuYLbiFgdAcZ-mKa22oeHOZuN2iFFYzKCeDBOA4ZgJX74pQo8EliICqLxXI7Jo3gOPElzU3O11CS4kxFMGylSW_vSW9v6lTimUGXz4aW6Te-VkWLUmzQrvOiWaObizOe_y1dK3CXwNfCp0mzh1cDbTmGpAiHG5ShRC4Du2sAPudcnobX9hrPRKp5Ly0M0AOVnpJtKfh1zyOzDWjwoY9qA97hoyd1ITfNg8ZBHHe3a_2gKh2Lj85OWNANgL8doeABAAAAAGBoJ6aAA"
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQCk-AgAXjv-rvP8Q3ZK7uVQ4bc6maTPaqtZ6iFqJDxWELtFu74RHoIvXfn6dLPZ9b1u9fWlV4ZqFJmWKm4jXrbw_LzfAeuYLbiFgdAcZ-mKa22oeHOZuN2iFFYzKCeDBOA4ZgJX74pQo8EliICqLxXI7Jo3gOPElzU3O11CS4kxFMGylSW_vSW9v6lTimUGXz4aW6Te-VkWLUmzQrvOiWaObizOe_y1dK3CXwNfCp0mzh1cDbTmGpAiHG5ShRC4Du2sAPudcnobX9hrPRKp5Ly0M0AOVnpJtKfh1zyOzDWjwoY9qA97hoyd1ITfNg8ZBHHe3a_2gKh2Lj85OWNANgL8doeABAAAAAGBoJ6aAA")
+
 
 # Initialize the string session client
 string_session_client = Client("my_session", api_id="10811400", api_hash="191bf5ae7a6c39771e7b13cf4ffd1279", session_string=Config.STRING_SESSION)
