071eb77093385e748f94dd7e0ee14531ee3239c3
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-05-29 09:30:14 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -10,7 +10,7 @@ from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.errors import MessageNotModified
 from config import DOWNLOAD_LOCATION, CAPTION
-from main.utils import progress_message, humanbytes
+from main.utils import progress_message, humanbytes, progress_pyrogram
 import subprocess
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import GROUP
@@ -20,9 +20,9 @@ FILE_SIZE_LIMIT = 2 * 1024 * 1024 * 1024  # 2 GB
 
 string_session_client = Client(
     "session_string",
-    api_id="YOUR_API_ID",
-    api_hash="YOUR_API_HASH",
-    session_string="YOUR_SESSION_STRING"
+    api_id="10811400",
+    api_hash="191bf5ae7a6c39771e7b13cf4ffd1279",
+    session_string="BQCk-AgAXjv-rvP8Q3ZK7uVQ4bc6maTPaqtZ6iFqJDxWELtFu74RHoIvXfn6dLPZ9b1u9fWlV4ZqFJmWKm4jXrbw_LzfAeuYLbiFgdAcZ-mKa22oeHOZuN2iFFYzKCeDBOA4ZgJX74pQo8EliICqLxXI7Jo3gOPElzU3O11CS4kxFMGylSW_vSW9v6lTimUGXz4aW6Te-VkWLUmzQrvOiWaObizOe_y1dK3CXwNfCp0mzh1cDbTmGpAiHG5ShRC4Du2sAPudcnobX9hrPRKp5Ly0M0AOVnpJtKfh1zyOzDWjwoY9qA97hoyd1ITfNg8ZBHHe3a_2gKh2Lj85OWNANgL8doeABAAAAAGBoJ6aAA"
 )
 
 
