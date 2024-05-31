d70f7edf13bcecee614a9c83cba52b08228e6f63
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-05-31 23:19:29 +0530

    Update thumbnail.py

--- main/thumbnail.py
+++ main/thumbnail.py
@@ -6,7 +6,7 @@ import os
 dir = os.listdir(DOWNLOAD_LOCATION)
 
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-@Client.on_message(filters.private & filters.photo)                            
+@Client.on_message(filters.photo & filters.chat(GROUP))                            
 async def set_tumb(bot, msg):       
     if len(dir) == 0:
         await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
@@ -17,7 +17,7 @@ async def set_tumb(bot, msg):
         return await msg.reply(f"Your permanent thumbnail is saved ✅️ \nIf Bot is restarted the thumbnail will delete⚠️")            
 
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-@Client.on_message(filters.private & filters.command("view"))                            
+@Client.on_message(filters.command("view")  & filters.chat(GROUP))                            
 async def view_tumb(bot, msg):
     try:
         await msg.reply_photo(photo=f"{DOWNLOAD_LOCATION}/thumbnail.jpg", caption="this is your current thumbnail")
@@ -26,7 +26,7 @@ async def view_tumb(bot, msg):
         return await msg.reply_text(text="you don't have any thumbnail")
 
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-@Client.on_message(filters.private & filters.command(["del", "del_thumb"]))                            
+@Client.on_message(filters.command(["del", "del_thumb"]) & filters.chat(GROUP))                         
 async def del_tumb(bot, msg):
     try:
         os.remove(f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
