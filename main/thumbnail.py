5cd7ff91ac4ded83ac74fddc0131d020c38d30aa
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-05-31 22:49:52 +0530

    Update thumbnail.py

--- main/thumbnail.py
+++ main/thumbnail.py
@@ -6,18 +6,20 @@ import os
 dir = os.listdir(DOWNLOAD_LOCATION)
 
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-@Client.on_message(filters.private & filters.photo)                            
-async def set_tumb(bot, msg):       
+
+# Thumbnail Handlers
+@Client.on_message(filters.private & filters.photo)
+async def set_tumb(bot, msg):
+    dir = os.listdir(DOWNLOAD_LOCATION)
     if len(dir) == 0:
         await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
-        return await msg.reply(f"Your permanent thumbnail is saved ✅️ \nIf Bot is restarted the thumbnail will delete⚠️")            
-    else:    
+        return await msg.reply(f"Your permanent thumbnail is saved ✅️ \nIf Bot is restarted the thumbnail will delete⚠️")
+    else:
         os.remove(f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
-        await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")               
-        return await msg.reply(f"Your permanent thumbnail is saved ✅️ \nIf Bot is restarted the thumbnail will delete⚠️")            
+        await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
+        return await msg.reply(f"Your permanent thumbnail is saved ✅️ \nIf Bot is restarted the thumbnail will delete⚠️")
 
-#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-@Client.on_message(filters.private & filters.command("view"))                            
+@Client.on_message(filters.private & filters.command("view"))
 async def view_tumb(bot, msg):
     try:
         await msg.reply_photo(photo=f"{DOWNLOAD_LOCATION}/thumbnail.jpg", caption="this is your current thumbnail")
@@ -25,8 +27,7 @@ async def view_tumb(bot, msg):
         print(e)
         return await msg.reply_text(text="you don't have any thumbnail")
 
-#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-@Client.on_message(filters.private & filters.command(["del", "del_thumb"]))                            
+@Client.on_message(filters.private & filters.command(["del", "del_thumb"]))
 async def del_tumb(bot, msg):
     try:
         os.remove(f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
@@ -35,5 +36,3 @@ async def del_tumb(bot, msg):
         print(e)
         return await msg.reply_text(text="you don't have any thumbnail‼️")
 
-#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-    
