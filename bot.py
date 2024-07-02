7c2e710483f3d7ff3522713590a712a45a61fac3
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-02 17:14:14 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="INFINITYSTARRENAME24BOT",
+            name="MetaMorpher",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
