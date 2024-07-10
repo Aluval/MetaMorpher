5673ed8dca8ffca4f9ce7d8bd970b665138068b9
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-10 10:27:03 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -22,12 +22,11 @@ from main.ffmpeg import remove_all_tags, change_video_metadata, generate_sample_
 
 DOWNLOAD_LOCATION1 = "./screenshots"
 
-# Initialize Gofile API key variable
-GOFILE_API_KEY = ""
+
 
 # Global dictionary to store user settings
 merge_state = {}
-user_gofile_api_keys = {}  # Dictionary to store Gofile API keys for each user
+GOFILE_API_KEY = {}  # Dictionary to store Gofile API keys for each user
 
 user_settings = {}
 
@@ -288,15 +287,15 @@ async def inline_preview_metadata_callback(_, callback_query):
 
 @Client.on_callback_query(filters.regex("^preview_gofilekey$"))
 async def inline_preview_gofile_api_key(bot, callback_query):
-    global user_gofile_api_keys
+    global GOFILE_API_KEY
     user_id = callback_query.from_user.id
     
     # Check if the API key is set for the user
-    if user_id not in user_gofile_api_keys:
+    if user_id not in GOFILE_API_KEY:
         return await callback_query.message.reply_text(f"Gofile API key is not set for user `{user_id}`. Use /gofilesetup {{your_api_key}} to set it.")
     
     # Reply with the current API key for the user
-    await callback_query.message.reply_text(f"Current Gofile API Key for user `{user_id}`: {user_gofile_api_keys[user_id]}")
+    await callback_query.message.reply_text(f"Current Gofile API Key for user `{user_id}`: {GOFILE_API_KEY[user_id]}")
     
 # Inline query handler for attaching photo
 @Client.on_callback_query(filters.regex("^attach_photo$"))
@@ -483,7 +482,7 @@ async def set_metadata_command(client, msg):
 # Command to set up Gofile API key
 @Client.on_message(filters.private & filters.command("gofilesetup"))
 async def gofile_setup(bot, msg: Message):
-    global user_gofile_api_keys  # Use global to modify the dictionary
+    global GOFILE_API_KEY  # Use global to modify the dictionary
 
     if len(msg.command) < 2:
         return await msg.reply_text("Please provide your Gofile API key.")
@@ -493,7 +492,7 @@ async def gofile_setup(bot, msg: Message):
     new_api_key = msg.command[1]
 
     # Set the API key for the user and confirm
-    user_gofile_api_keys[user_id] = new_api_key
+    GOFILE_API_KEY[user_id] = new_api_key
     await msg.reply_text(f"Gofile API key set successfully for user `{user_id}`✅!")
 
 
