c499dd9f548651e852deda0e57c1195edea99a13
Hsksv639 <Bfarbqhfq@outlook.com>
2024-07-11 16:45:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(os.environ.get("API_ID", "10811400"))
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7148094317:AAG7zC7zvc_XeEkA9y3-G_rlP8HgLkJyzaM")
+ADMIN = int(os.environ.get("ADMIN", '6874351976')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-group = environ.get('GROUP', '-1002128043143')
+group = environ.get('GROUP', '')
 GROUP = int(group) if group and id_pattern.search(group) else None
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
 AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
