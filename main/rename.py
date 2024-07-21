05cf964d9468ae231b8b03d34313c325bad2a249
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-21 19:12:41 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- main/rename.py
+++ main/rename.py
@@ -948,6 +948,7 @@ async def change_index_audio(bot, msg):
             await bot.send_document(
                 msg.chat.id,
                 document=output_file,
+                file_name=output_filename,  # Apply the new file name here
                 thumb=file_thumb,
                 caption=cap,
                 progress=progress_message,
@@ -962,7 +963,6 @@ async def change_index_audio(bot, msg):
     if file_thumb and os.path.exists(file_thumb):
         os.remove(file_thumb)
     await sts.delete()
-
 
 #changeindex subtitles 
 # Command to change index subtitle
