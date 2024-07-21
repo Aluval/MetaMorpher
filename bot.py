2c2415226e9dd69c40679adf2fb83a01d073f120
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-21 11:28:27 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()        
-        await self.send_message(LOG_CHANNEL_ID, f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
         
                 
         
