ca009fe7ee95e25a7572b5eee0bb1e1e7bee13ac
nandureigns <139434750+nandureigns@users.noreply.github.com>
2024-09-14 11:18:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+API_ID = int(os.environ.get("API_ID", "24464839"))
+API_HASH = os.environ.get("API_HASH", "c906bdd79dae0c7e6b8446db37128705")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6759334684:AAEzkcL0oBzKJUrm1JLipJNVA3S6TPb5HDw")
+ADMIN = int(os.environ.get("ADMIN", '5806876587')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "knmoviez")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "logsforkn")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-group = environ.get('GROUP', '-1002173560131')
+group = environ.get('GROUP', '-1002144228393')
 GROUP = int(group) if group and id_pattern.search(group) else None
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", ''))
 
 
 
