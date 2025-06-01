f1d840b4b4f8bf7b76ddf77c202b880a17f81f64
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-06-01 18:16:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ GROUP = int(group) if group and id_pattern.search(group) else None
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
 AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-PORT = int(os.environ.get("PORT", "8080"))
+PORT = int(os.environ.get("PORT", "8081")) #8080 for Koyeb
 LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
