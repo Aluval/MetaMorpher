4722f7d2faa28d6ea6a0fa76a9ca77d9aa32dab5
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2026-04-19 16:32:22 +0530

    Update BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGozPuqq2JOXMYYC4_nZpDMQDsJg-fegX0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAEF8UQr_S95uOnqTyNVEfGttDwjRrZ8X1Y")
 ADMIN = int(os.environ.get("ADMIN", '6469754522'))
 FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
 FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
